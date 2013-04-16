ffcd29ffd616fd56c1de248aa8c17ef416951ab7
li25705199 <25705199@qq.com>
2013-04-16 11:56:11 +0800

    测试分支4

--- src/com/my/fragments/MainActivity.java
+++ src/com/my/fragments/MainActivity.java
@@ -21,5 +21,17 @@ public class MainActivity extends Activity {
         // TODO Auto-generated method stub
         super.onPause();
     }
+    
+    @Override
+    protected void onRestart() {
+        // TODO Auto-generated method stub
+        super.onRestart();
+    }
+    
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+    }
 
 }
