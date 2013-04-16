8ef482c3df6f54ed3cabe60b8356759415eb25db
li25705199 <25705199@qq.com>
2013-04-16 11:49:06 +0800

    测试分支3

--- src/com/my/fragments/MainActivity.java
+++ src/com/my/fragments/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         Log.e("", "");
+        Log.v("", "");
     }
     
     @Override
