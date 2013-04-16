741302af8694065ce60955848002b21504f526c7
alvin87 <alvinli87@gmail.com>
2013-04-16 11:48:44 +0800

    测试主干3

--- src/com/my/fragments/MainActivity.java
+++ src/com/my/fragments/MainActivity.java
@@ -4,6 +4,7 @@ import com.example.fragmentsdemo.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        Log.d("", "");
 
     }
     
