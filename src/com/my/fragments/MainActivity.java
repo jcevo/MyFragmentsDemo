49c4f76c7cc735786beeec78b83d3c1f276ef298
li25705199 <25705199@qq.com>
2013-04-16 12:22:39 +0800

    测试分支7

--- src/com/my/fragments/MainActivity.java
+++ src/com/my/fragments/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity {
     protected void onResume() {
         // TODO Auto-generated method stub
         super.onResume();
-        Log.e("", "");
+        Log.e("", "ss");
     }
 
     
